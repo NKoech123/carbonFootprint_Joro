b097b4ec5b6be06ddf18e8170f7f487354e1c47b
Nicholas Koech <nicholaskoech@Nicholass-MacBook-Pro.local>
2022-11-03 22:55:26 -0700

    clean app.js

--- app.js
+++ app.js
@@ -5,32 +5,44 @@ const { getFootprint, getTransactions, getFootprintPerDayTransactionData } = req
 const {insertTransactions} = require("./loadData")
 const { Transaction } = require("./models");
 
-// Endpoint to load data from external API into DB
 app.get('/insertTransactions', async (req, res) => {
-
-  const transationData = await insertTransactions();
   try{
+    const transationData = await insertTransactions();
     return res.json(transationData);
   }catch(err){
     console.log(err)
     return res.status(500).json(err)
-   }
- 
+  }
 });
 
 app.get('/footprint', async(req, res) => {
+  try{
   const footprintObject = await getFootprint();
   res.json(footprintObject);
+}catch(err){
+  console.log(err)
+  return res.status(500).json(err)
+}
 });
 
 app.get('/footprintPerDay', async(req, res) => {
+  try{
   const footprintPerDayObject = await getFootprintPerDayTransactionData();
   res.json(footprintPerDayObject);
+}catch(err){
+  console.log(err)
+  return res.status(500).json(err)
+}
 });
 
 app.get('/transactions', async (req, res) => {
+  try{
   const transactionsObject = await getTransactions();
   res.json(transactionsObject);
+}catch(err){
+  console.log(err)
+  return res.status(500).json(err)
+}
 });
 
 app.get('/', (req, res) => res.json({ server_status: 'working' }))
